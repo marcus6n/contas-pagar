114204f47aa144f112c0f1a08c75ada8c3012311
marcus6n <marcusdeoliveiraneves@gmail.com>
2025-11-04 19:29:33 -0300

    first commit

---
+++ src/main/java/br/com/deliverit/contas_pagar/ContasPagarApplication.java
@@ -0,0 +1,13 @@
+package br.com.deliverit.contas_pagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasPagarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasPagarApplication.class, args);
+	}
+
+}
